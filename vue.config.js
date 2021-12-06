8da58199cd19c996163e757d0e87143a22fea13e
wangxiang <1827945911@qq.com>
2021-12-07 00:05:31 +0800

    打包发布

--- vue.config.js
+++ vue.config.js
@@ -10,7 +10,16 @@ Released under the MIT License`
 
 module.exports = {
 
+  // 部署生产环境和开发环境下的URL。
+  // 默认情况下，Vue CLI 会假设你的应用是被部署在一个域名的根路径上
+  // 例如 https://www.entfrm.com/。如果应用被部署在一个子路径上，你就需要用这个选项指定这个子路径。例如，如果你的应用被部署在 https://www.entfrm.com/admin/，则设置 baseUrl 为 /admin/。
+  publicPath: process.env.NODE_ENV === "production" ? "./" : "/",
+
+  // 如果你不需要生产环境的 source map，可以将其设置为 false 以加速生产环境构建。
   productionSourceMap: false,
+
+  // 用于放置生成的静态资源 (js、css、img、fonts) 的；（项目打包之后，静态资源会放在这个文件夹下）
+  assetsDir: 'static',
 
   configureWebpack: {
     output: {
