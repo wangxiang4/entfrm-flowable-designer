85909fde191905e2e1281bcbf725412f0ff2edc4
wangxiang <1827945911@qq.com>
2022-01-06 05:35:03 +0800

    重新发布

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,6 @@
 <template>
   <div id="app">
-    <el-dialog title="设计流程模型"
-               center
-               fullscreen
-               class="design"
-               :visible="true"
-               :close-on-click-modal="false"
-    >
-      <flowable-designer style="height:calc(100vh - 57px)"/>
-    </el-dialog>
+    <flowable-designer/>
     <!--<flowable-chart/>-->
     <!--<flowable-preview/>-->
   </div>
